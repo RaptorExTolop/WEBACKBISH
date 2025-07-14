b28ba8cd88162ba675a770e062d25c671a299c82
RaptorExTolop <jamesappletonret@gmail.com>
2025-07-15 07:40:22 +0930

    WE GOT TILES LOADED LES GOOOO Todo: get tiles to work from a file path, add offset, add collisions

--- tiles.go
+++ tiles.go
@@ -17,25 +17,23 @@ func (TS *TileSet) loadTiles(filePath string, tileSize float32) {
 	TS.filePath = filePath
 	fmt.Println(TS.filePath)
 	TS.tiles = [][]float32{
-		{1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 1},
-		{1, 0, 0, 0, 1},
-		{1, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1},
 	}
 }
 
 func (TS *TileSet) drawTiles() {
-	tileSize := rl.Vector2{X: TS.tileSize, Y: TS.tileSize}
-	for x, tiles := range TS.tiles {
-		fmt.Println("X", x)
-		for y := range tiles {
-			tileLocation := rl.Vector2{X: float32(x) * TS.tileSize, Y: float32(y) * TS.tileSize}
-			switch y {
-			case 1:
-				rl.DrawRectangleV(tileLocation, tileSize, rl.Color{R: 0, G: 0, B: 0, A: 255})
-			case 0:
-				rl.DrawRectangleV(tileLocation, tileSize, rl.Color{R: 0, G: 0, B: 0, A: 0})
+	for y, tiles := range TS.tiles {
+		for x, tile := range tiles {
+			if tile == 1 {
+				rl.DrawRectangle(int32(x*int(TS.tileSize)), int32(y*int(TS.tileSize)), int32(TS.tileSize), int32(TS.tileSize), rl.Black)
 			}
 		}
 	}
